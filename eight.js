a70706877c7b44fbb496bc2715d2c283ebf2f2a3
disha100 <103488065+disha100@users.noreply.github.com>
2022-09-17 23:42:09 +0530

    Add files via upload

---
+++ eight.js
@@ -0,0 +1,17 @@
+//object literals
+
+var pen ={
+    type:"gel",
+    company:"flair",
+    colour:"blue",
+    owner:{
+        name:"john",
+        age:34,
+        gender:"Male",
+
+    },
+    cost:10,
+};
+console.log(pen);
+console.log(pen.owner.name);
+console.log(pen.cost);
