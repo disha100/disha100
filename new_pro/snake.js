eeb86eb9d91fd6f53c6d2ac260b9c7e9b146f9a8
disha100 <103488065+disha100@users.noreply.github.com>
2022-11-03 00:17:24 +0530

    Update snake.js
    ..

--- new_pro/snake.js
+++ new_pro/snake.js
@@ -17,3 +17,18 @@ for(var i=0;i<N;i++){
         ctx.fillRect(i*cellSize,j*cellSize,cellSize,cellSize);
     }
 }
+function handleKey(e){
+    e=e||window.event;
+    if (e.keyCode == '38'){
+ }
+    else if (e.keyCode == '40'){
+ }
+    else if (e.keyCode == '37'){
+ }
+    else if (e.keyCode == '39'){
+ }
+    console.log(e.keyCode);
+
+
+}
+document.onkeydown = handleKey;
