ded1facac8445708d532302ff981ab05f2f20618
disha100 <103488065+disha100@users.noreply.github.com>
2022-11-21 23:42:43 +0530

    moving files

---
+++ files_here/five.js
@@ -0,0 +1,13 @@
+//Boolean true or false values
+var a=( "true " );
+if(a==null){
+console.log(false);
+}
+else{
+    console.log("yes ");
+}
+//
+false
+//false||null||undefined||""||0||NaN
+true
+//"hello"||1||-1||"true"||"anything"
