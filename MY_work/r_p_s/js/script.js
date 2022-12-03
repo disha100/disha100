7d84d1adc8f55c518ebd319eb39a5d129f6c19d0
disha100 <103488065+disha100@users.noreply.github.com>
2022-12-04 00:01:24 +0530

    js file updated

--- MY_work/r_p_s/js/script.js
+++ MY_work/r_p_s/js/script.js
@@ -1,18 +1,7 @@
-/*
-  Rock Paper Scissors SOLUTION 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
-*/
+
 
 // ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
-// getComputerChoice() 👉 'Rock'
-// getComputerChoice() 👉 'Scissors'
+
 function getComputerChoice() {
   let rpsChoices = ['Rock', 'Paper', 'Scissors']
   let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
@@ -20,9 +9,7 @@ function getComputerChoice() {
 }
 
 // ** getResult compares playerChoice & computerChoice and returns the score accordingly **
-// human wins - getResult('Rock', 'Scissors') 👉 1
-// human loses - getResult('Scissors', 'Rock') 👉 -1
-// human draws - getResult('Rock', 'Rock') 👉 0
+
 function getResult(playerChoice, computerChoice) {
   // return the result of score based on if you won, drew, or lost
   
@@ -53,9 +40,7 @@ function getResult(playerChoice, computerChoice) {
 
 // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
 function showResult(score, playerChoice, computerChoice) {
-  // Hint: on a score of -1
-  // You should do result.innerText = 'You Lose!'
-  // Don't forget to grab the div with the 'result' id!
+
   
   let result = document.getElementById('result')
   switch (score) {
